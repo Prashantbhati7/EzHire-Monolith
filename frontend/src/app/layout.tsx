60479157545937ddd7116f4dd7f8fd88e29a0448
Prashant Bhati <prashantbhati774@gmail.com>
2026-03-22 10:24:46 +0530

    feat: Add new EzHire logo, update favicon, and configure Open Graph and Twitter metadata.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -10,6 +10,28 @@ import { LoaderIcon } from "lucide-react";
 export const metadata: Metadata = {
   title: "EzHire",
   description: "To simplify the hiring and applying process.",
+  openGraph: {
+    title: "EzHire",
+    description: "To simplify the hiring and applying process.",
+    url: "https://ez-hire-monolith.vercel.app", // Replace with your actual domain
+    siteName: "EzHire",
+    images: [
+      {
+        url: "/EzHireLogo.webp", // Path to your OG image in the public folder
+        width: 1200,
+        height: 630,
+        alt: "EzHire Preview Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "EzHire",
+    description: "To simplify the hiring and applying process.",
+    images: ["/EzHireLogo.webp"], // Path to your Twitter image in the public folder
+  },
 };
 
 export default function RootLayout({
